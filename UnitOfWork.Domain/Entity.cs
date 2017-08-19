e7c486e9b691b3f61c435d51bc5325da1833271c
shengjie_yan <ysjshengjie@live.cn>
2017-08-19 20:45:17 +0800

    定义实体（层超类型）

---
+++ UnitOfWork.Domain/Entity.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+namespace UnitOfWork
+{
+    public class Entity : Entity<int>, IEntity
+    {
+    }
+
+    public class Entity<TPrimaryKey> : IEntity<TPrimaryKey>
+    {
+        public virtual TPrimaryKey Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity<TPrimaryKey>))
+            {
+                return false;
+            }
+
+            //Same instances must be considered as equal
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            var other = (Entity<TPrimaryKey>)obj;
+            //Must have a IS-A relation of types or must be same type
+            var typeOfThis = GetType();
+            var typeOfOther = other.GetType();
+            if (!typeOfThis.GetTypeInfo().IsAssignableFrom(typeOfOther) && !typeOfOther.GetTypeInfo().IsAssignableFrom(typeOfThis))
+            {
+                return false;
+            }
+
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+        {
+            if (Equals(left, null))
+            {
+                return Equals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ UnitOfWork.Domain/IEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace UnitOfWork
+{
+    /// <summary>
+    /// A shortcut of <see cref="IEntity{TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
+    /// </summary>
+    public interface IEntity
+    {
+        
+    }
+
+    /// <summary>
+    /// Defines interface for base entity type. All entities in the system must implement this interface.
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">Type of the primary key of the entity</typeparam>
+    public interface IEntity<TPrimaryKey>
+    {
+        /// <summary>
+        /// Unique identifier for this entity.
+        /// </summary>
+        TPrimaryKey Id { get; set; }
+    }
+
+
+}
